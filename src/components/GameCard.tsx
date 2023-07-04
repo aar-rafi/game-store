5b000188fe0d866d3bf08c7092770646a248de56
Rafi <ssrabonislam2000@gmail.com>
2023-07-04 16:36:01 +0600

    cleanups

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,13 +14,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody bgColor={"blackAlpha.300"}>
-        <Heading fontSize={"x-large"}>{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIcons
             platform={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize={"x-large"}>{game.name}</Heading>
       </CardBody>
     </Card>
   );
